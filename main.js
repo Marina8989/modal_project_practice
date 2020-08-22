dfd2fc0639793dfe3d398c6f18cebf814b6c1628
Marina8989 <marina.mill209@gmail.com>
2020-08-21 19:46:37 -0700

    js practice

---
+++ main.js
@@ -0,0 +1,4 @@
+//selelct modal-btn, modal-overelay, close-btn
+//listen for click events on modal-btn and close-btn
+//when user clicks modal-btn add .OPEN-MODAL to modal-overlay
+//when user clicks close-btn remove .OPEN-MODAL from modal-overlay
